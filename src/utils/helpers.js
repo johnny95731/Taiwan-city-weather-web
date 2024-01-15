568d757d423f9ae83edd237afac21b0349103d36
johnny95731 <johnny95731@yahoo.com>
2024-01-15 18:10:49 +0800

    修改日出日落判斷

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,4 @@
-import sunriseAndSunsetData from "./sunrise-sunset.json";
+import sunriseSunsetData from "./sunrise-sunset.json";
 import locations from './locations.json';
 
 export const getLocation = (cityName) => {
@@ -31,50 +31,37 @@ export const getTowns = (cityName) => {
   return locations[cityName].towns
 };
 
-export const getMoment = (locationName) => {
-  /**
-   * Return current time is day or night in a districts.
-   */
-  // From sunrise-sunset data get local data.
-  const location = sunriseAndSunsetData.find(
-    (data) => data.locationName === locationName
-  );
-  if (!location) {
-    throw new Error(`找不到 ${location} 的日出日落資料`);
-  }
-
+export const getMoment = () => {
   // Get local time and fromat as yyyy-mm-dd
   const now = new Date();
   const nowDate = Intl.DateTimeFormat('zh-TW', {
-    year: 'numeric',
     month: '2-digit',
     day: '2-digit',
   })
     .format(now)
     .replace(/\//g, '-');
-
+  const year = now.getFullYear()
   // Find sunrise-sunset moment of today.
-  const locationDate = location?.time.find((time) => time.dataTime === nowDate);
+  const locationDate = sunriseSunsetData.find((time) => time.date === nowDate);
   if (!locationDate) {
-    throw new Error(`找不到 ${locationName} 在 ${nowDate} 的日出日落資料`);
+    throw new Error(`找不到在 ${nowDate} 的日出日落資料`);
   }
 
   // Convert sunrise, sunset, and nowTime to TimeStamp
   const sunriseTimestamp = new Date(
-    `${locationDate.dataTime} ${locationDate.sunrise}`
+    `${year}-${locationDate.date} ${locationDate.sunrise}`
   ).getTime();
   const sunsetTimestamp = new Date(
-    `${locationDate.dataTime} ${locationDate.sunset}`
+    `${year}-${locationDate.date} ${locationDate.sunset}`
   ).getTime();
   const nowTimeStamp = now.getTime();
-
   // Verifying current time is day or night.
   return sunriseTimestamp <= nowTimeStamp && nowTimeStamp <= sunsetTimestamp
     ? 'day'
     : 'night';
 };
 
-const rgbCoeff = [0.299, 0.587, 0.114];
+const RGB2GRAY_COEFF = [0.299, 0.587, 0.114];
 export const hex2Decimal = (hex) => {
   /**
    * Convert hex rgb color to gray scale number.
@@ -84,12 +71,12 @@ export const hex2Decimal = (hex) => {
   }
   if ( hex.length === 3 ) {
     const strs = hex.split("");
-    const vals = strs.map((str, i) => parseInt(str+str, 16) * rgbCoeff[i]);
+    const vals = strs.map((str, i) => parseInt(str+str, 16) * RGB2GRAY_COEFF[i]);
     return vals.reduce((cummul, val) => cummul += val, 0);
   } else if ( hex.length === 6 ) {
-    return parseInt(hex.substr(0, 2), 16) * rgbCoeff[0]
-         + parseInt(hex.substr(2, 2), 16) * rgbCoeff[1]
-         + parseInt(hex.substr(4, 2), 16) * rgbCoeff[2]
+    return parseInt(hex.substr(0, 2), 16) * RGB2GRAY_COEFF[0]
+         + parseInt(hex.substr(2, 2), 16) * RGB2GRAY_COEFF[1]
+         + parseInt(hex.substr(4, 2), 16) * RGB2GRAY_COEFF[2]
   }
   return null
 };
